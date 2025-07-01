42e40d2d859badda380a2bce8376f2cd339345b3
Fe-r-oz <feroz.ahmad.email@gmail.com>
2025-07-01 18:01:40 +0500

    🛠️ include quantum_tanner_code.jl file in quantum_expanders.jl so it's methods correctly loaded

--- src/QuantumExpanders.jl
+++ src/QuantumExpanders.jl
@@ -14,6 +14,7 @@ using Graphs:add_edge!,nv,ne,neighbors,Graphs
 
 include("morgenstern.jl")
 include("cayley_graphs.jl")
+include("quantum_tanner_code.jl")
 include("tensor_codes.jl")
 
 export gen_code, gen_good_code, tanner_code, tanner_code_quadripartite, cayley_complex_square_graphs, cayley_complex_square_graphs_quadripartite, morgenstern_generators, alternative_morgenstern_generators
