a2eef488fe5f205aeaedf942a89e59ff55b59735
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-10-01 09:41:37 -0400

    add documentation setup

---
+++ docs/make.jl
@@ -0,0 +1,27 @@
+push!(LOAD_PATH,"../src/")
+
+using Documenter
+#using DocumenterCitations
+using QuantumExpanders
+
+#bib = CitationBibliography(joinpath(@__DIR__,"src/references.bib"))
+
+ENV["LINES"] = 80    # for forcing `displaysize(io)` to be big enough
+ENV["COLUMNS"] = 80
+
+bib = CitationBibliography(joinpath(@__DIR__,"src/references.bib"),style=:authoryear)
+
+makedocs(
+#bib,
+doctest = false,
+clean = true,
+warnonly = :missing_docs,
+sitename = "QuantumExpanders.jl",
+format = Documenter.HTML(),
+authors = "Feroz Ahmed Mian",
+linkcheck = true,
+)
+
+deploydocs(
+    repo = "github.com/QuantumSavory/QuantumExpanders.jl.git"
+)
