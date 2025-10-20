c3e50feef108cf1f3cb195cabab553ac85078de9
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-10-20 05:26:23 -0400

    fix typo

--- test/test_quantum_tanner_codes.jl
+++ test/test_quantum_tanner_codes.jl
@@ -25,7 +25,7 @@
                 A_pairs = alternative_morgenstern_generators(B, AllPairs())
                 A, G = A_pairs, SL₂
                 squares = enumerate_square_incidences(G, A, B)
-                classical_code_pair = random_code_pair(rho, delta)
+                classical_code_pair = random_code_pair(ρ, Δ)
                 hx, hz = parity_matrix(length(G), squares, classical_code_pair)
                 iszero(mod.(hx*hz',2))
             end
