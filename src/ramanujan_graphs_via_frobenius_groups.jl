931975c42a70f0c5174bb5d9f57ad51ba1de9a16
Fe-r-oz <feroz.ahmed.mian@gmail.com>
2025-11-13 23:53:50 -0500

    polish

--- src/ramanujan_graphs_via_frobenius_groups.jl
+++ src/ramanujan_graphs_via_frobenius_groups.jl
@@ -80,25 +80,3 @@ function normal_cayley_subset(G::Group)
     S = union(non_trivial_classes...)
     return collect(S)
 end
-
-function adjacency_matrix(g::SimpleGraph)
-    n = nv(g)
-    A = zeros(Int, n, n)
-    for e in edges(g)
-        i, j = src(e), dst(e)
-        A[i, j] = 1
-        A[j, i] = 1
-    end
-    return A
-end
-
-function is_ramanujan(g, k)
-    A = adjacency_matrix(g)
-    evals = real.(eigvals(A))
-    sorted_evals = sort(evals, rev=true)
-    λ1 = sorted_evals[1]
-    non_trivial_evals = sorted_evals[2:end]
-    μ = maximum(abs.(non_trivial_evals))
-    ramanujan_bound = 2 * sqrt(k - 1)
-    return μ <= ramanujan_bound + 1e-10
-end
